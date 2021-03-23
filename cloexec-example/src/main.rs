d40ad68407f90f44252dc6621edeaba4f1fda5ea
Uchio Kondo <udzura@udzura.jp>
2021-03-23 20:58:04 +0900

    Impl

--- cloexec-example/src/main.rs
+++ cloexec-example/src/main.rs
@@ -1,10 +1,12 @@
+use nix::fcntl::*;
 use nix::sys::wait::waitpid;
 use nix::unistd::{close, execvp};
 use nix::unistd::{fork, ForkResult};
 use std::env::args;
 use std::ffi::CString;
+use std::io::{Read, Write};
 use std::net::Shutdown;
-use std::os::unix::io::IntoRawFd;
+use std::os::unix::io::{FromRawFd, IntoRawFd, RawFd};
 use std::os::unix::net::*;
 use std::thread::sleep;
 use std::time::Duration;
@@ -12,25 +14,54 @@ use std::time::Duration;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     if &args().nth(0).unwrap() == "child" {
         println!("I'm a new child process");
-        sleep(Duration::from_secs(30));
+        let sock2: RawFd = args().nth(1).unwrap().parse()?;
+        let mut sock2 = unsafe { UnixStream::from_raw_fd(sock2) };
+
+        loop {
+            let mut buf = [0u8; 1024];
+            if sock2.read(&mut buf)? == 0 {
+                break;
+            }
+            print!("{}", String::from_utf8((&buf).to_vec())?.trim_matches('\0'));
+        }
+        println!("All drained.");
+        sleep(Duration::from_secs(3));
+
         return Ok(());
     }
 
     let (sock1, sock2) = UnixStream::pair()?;
-    match unsafe { fork()? } {
+    let sock = match unsafe { fork()? } {
         ForkResult::Parent { child } => {
-            sock2.shutdown(Shutdown::Both)?;
-            close(sock2.into_raw_fd())?;
             println!("Added child. Pid: {}", child);
+            close(sock2.into_raw_fd())?;
+            sock1.into_raw_fd()
         }
         ForkResult::Child => {
             let cmd = CString::new("/proc/self/exe").unwrap();
-            let args = vec![CString::new("child").unwrap()];
-            sock1.shutdown(Shutdown::Both)?;
             close(sock1.into_raw_fd())?;
+
+            let newsock = sock2.into_raw_fd();
+            let _r = fcntl(newsock, FcntlArg::F_SETFD(FdFlag::empty()))?;
+
+            let args = vec![
+                CString::new("child").unwrap(),
+                CString::new(format!("{}", newsock)).unwrap(),
+            ];
             execvp(&cmd, &args.as_ref())?;
+            0
         }
+    };
+
+    let mut sock1 = unsafe { UnixStream::from_raw_fd(sock) };
+    for i in 0..5 {
+        for _ in 0..=i {
+            sock1.write(b"byte ")?;
+        }
+        sock1.write(b"\n")?;
+        sleep(Duration::from_secs(1));
     }
+    sock1.shutdown(Shutdown::Both)?;
 
     while let Ok(status) = waitpid(None, None) {
         println!("Reaped child. Status: {:?}", status);
