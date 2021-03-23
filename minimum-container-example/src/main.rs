ba52b48fd5ce457b7ad7684735fb89a6c7e12e3f
Uchio Kondo <udzura@udzura.jp>
2021-03-23 18:14:07 +0900

    It works

--- minimum-container-example/src/main.rs
+++ minimum-container-example/src/main.rs
@@ -1,18 +1,57 @@
+use nix::mount::*;
 use nix::sched::*;
 use nix::sys::wait::waitpid;
-use nix::unistd::execvp;
+use nix::unistd::{chroot, execvp};
+use std::env::{args, set_current_dir};
 use std::ffi::CString;
+type MyResult = Result<(), Box<dyn std::error::Error>>;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let cb = Box::new(&|| {
-        if let Ok(cmd) = CString::new("bash") {
-            let args = vec![
-                CString::new("containered bash").unwrap(),
-                CString::new("-l").unwrap(),
-            ];
-            if execvp(&cmd, &args.as_ref()).is_err() {
-                return 127;
-            }
+fn mount_make_private() -> Result<(), nix::Error> {
+    mount(
+        Some("none"),
+        "/",
+        None::<&str>,
+        MsFlags::MS_REC | MsFlags::MS_PRIVATE,
+        None::<&str>,
+    )
+}
+
+fn mount_proc(source: &str, target: &str) -> Result<(), nix::Error> {
+    mount(
+        Some(source),
+        target,
+        Some("proc"),
+        MsFlags::empty(),
+        None::<&str>,
+    )
+}
+
+fn container_prelude(root: &str) -> MyResult {
+    mount_make_private()?;
+    chroot(root)?;
+    set_current_dir("/")?;
+    mount_proc("proc", "/proc")?;
+    Ok(())
+}
+
+fn main() -> MyResult {
+    let usage = format!("Usage: {} [newroot]", args().nth(0).unwrap());
+    let root = args().nth(1).ok_or(usage)?;
+
+    let cb = Box::new(move || {
+        if let Err(e) = container_prelude(&root) {
+            eprintln!("mount failed: {:?}", e);
+            return 127;
+        }
+
+        let cmd = CString::new("bash").unwrap();
+        let args = vec![
+            CString::new("containered bash").unwrap(),
+            CString::new("-l").unwrap(),
+        ];
+        if let Err(e) = execvp(&cmd, &args.as_ref()) {
+            eprintln!("execvp failed: {:?}", e);
+            return 127;
         }
         127
     });
